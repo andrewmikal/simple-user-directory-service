2fd66b070e4e5d9de374daebcf3fa5a74177470a
Andrew Mika <ajtrex@gmail.com>
2019-10-23 11:01:02 -0400

    added has() method.

--- src/main/java/com/ajmi/simpleuserdirectoryservice/user/UserDirectory.java
+++ src/main/java/com/ajmi/simpleuserdirectoryservice/user/UserDirectory.java
@@ -1,4 +1,12 @@
 package com.ajmi.simpleuserdirectoryservice.user;
 
 public interface UserDirectory {
+
+    /**
+     * Checks if the specified user exists in the directory.
+     * @param username of the user to check for.
+     * @return true if the user was found in the directory, false otherwise.
+     */
+    boolean has(String username);
+
 }
