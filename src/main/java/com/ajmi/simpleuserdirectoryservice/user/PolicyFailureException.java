95c55830216a58aaa8cafc6834d5f2067b6f5f37
Andrew Mika <ajtrex@gmail.com>
2019-10-29 19:58:52 -0400

    Removed space from PolicyFailureException java doc.

--- src/main/java/com/ajmi/simpleuserdirectoryservice/user/PolicyFailureException.java
+++ src/main/java/com/ajmi/simpleuserdirectoryservice/user/PolicyFailureException.java
@@ -2,7 +2,7 @@ package com.ajmi.simpleuserdirectoryservice.user;
 
 /**
  * Exception thrown when the username, email, screen name, or password used to add a user to a UserDirectory does not
- *  meet the corresponding policy.
+ * meet the corresponding policy.
  */
 public class PolicyFailureException extends UserDirectoryException{
     public PolicyFailureException(String message) {
