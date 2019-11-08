7b82a4532374b63861c8c42dec7a530f9ce1545a
Andrew Mika <ajtrex@gmail.com>
2019-11-08 10:28:03 -0500

    Added instance variables and constructor for UserData.

--- src/main/java/com/ajmi/simpleuserdirectoryservice/user/UserData.java
+++ src/main/java/com/ajmi/simpleuserdirectoryservice/user/UserData.java
@@ -5,4 +5,22 @@ package com.ajmi.simpleuserdirectoryservice.user;
  */
 public class UserData {
 
+    /** Username of the user. */
+    private final String _username;
+    /** Email of the user. */
+    private final String _email;
+    /** Screen name of the user. */
+    private final String _screenName;
+
+    /**
+     * Create a new UserDirectory object from the provided information.
+     * @param username Username of the user.
+     * @param email Email of the user.
+     * @param screenName Screen name of the user.
+     */
+    public UserData(String username, String email, String screenName) {
+        _username = username;
+        _email = email;
+        _screenName = screenName;
+    }
 }
