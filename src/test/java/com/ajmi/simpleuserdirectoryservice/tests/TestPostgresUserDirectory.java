15699036e89b3e7507e926bd26f9d2a189811b09
Andrew Mika <ajtrex@gmail.com>
2019-12-04 16:31:25 -0500

    Created Test Class for PostgresUserDirectory.

---
+++ src/test/java/com/ajmi/simpleuserdirectoryservice/tests/TestPostgresUserDirectory.java
@@ -0,0 +1,11 @@
+package com.ajmi.simpleuserdirectoryservice.tests;
+
+import com.ajmi.simpleuserdirectoryservice.user.PostgresUserDirectory;
+import com.ajmi.simpleuserdirectoryservice.user.UserDirectory;
+
+public class TestPostgresUserDirectory extends TestUserDirectory {
+    @Override
+    protected UserDirectory create() {
+        return null;
+    }
+}
