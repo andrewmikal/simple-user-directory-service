b85c7500c855de26cb09a931f14431df76e70817
Andrew Mika <ajtrex@gmail.com>
2020-01-11 11:51:54 -0500

    Added UNDEINED to PolicyFailure, added PolicyFailure field failure to PolicyFailureException, added new constructors to PolicyFailureException supporting setting the failure, added getFailure() to PolicyFailureException to retreive the failure.

--- src/main/java/com/ajmi/simpleuserdirectoryservice/user/PolicyFailure.java
+++ src/main/java/com/ajmi/simpleuserdirectoryservice/user/PolicyFailure.java
@@ -4,6 +4,7 @@ package com.ajmi.simpleuserdirectoryservice.user;
  * Enum indicating what policy failed in a PolicyFailureException.
  */
 public enum PolicyFailure {
+    UNDEFINED,
     ILLEGAL_USERNAME,
     ILLEGAL_EMAIL,
     ILLEGAL_SCREEN_NAME,
