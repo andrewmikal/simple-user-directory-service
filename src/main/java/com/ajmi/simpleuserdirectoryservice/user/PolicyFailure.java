57d4afaaca7122242c4af1f6e862ee8f3831b91a
Andrew Mika <ajtrex@gmail.com>
2020-01-11 11:37:18 -0500

    Added enum PolicyFailure indicating the failed policy.

---
+++ src/main/java/com/ajmi/simpleuserdirectoryservice/user/PolicyFailure.java
@@ -0,0 +1,11 @@
+package com.ajmi.simpleuserdirectoryservice.user;
+
+/**
+ * Enum indicating what policy failed in a PolicyFailureException.
+ */
+public enum PolicyFailure {
+    ILLEGAL_USERNAME,
+    ILLEGAL_EMAIL,
+    ILLEGAL_SCREEN_NAME,
+    ILLEGAL_PASSWORD
+}
